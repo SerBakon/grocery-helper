47f17c7e1c9687899fc29d013d504a96675065ae
SerBakon <134323205+SerBakon@users.noreply.github.com>
2026-02-11 23:05:21 -0800

    feat: interactive selector

--- apps/web/app/_react-components/daily-chooser.tsx
+++ apps/web/app/_react-components/daily-chooser.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { listGroceries } from "../_rpc-client/rpc-client";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { set } from "mongoose";
 
 export default function DailyChooser() {
 	const [groceries, setGroceries] = useState<string[]>([]);
+	const [weeklyList, setWeeklyList] = useState<Set<string>>(new Set());
 
 	useEffect(() => {
 		let isMounted = true;
@@ -19,6 +22,23 @@ export default function DailyChooser() {
 			isMounted = false;
 		};
 	}, []);
+
+	const updateWeeklyList = (grocery: string) => {
+		if (weeklyList.has(grocery)) {
+			setWeeklyList((prev) => {
+				const next = new Set(prev);
+				next.delete(grocery);
+				return next;
+			});
+		} else {
+			setWeeklyList((prev) => {
+				const next = new Set(prev);
+				next.add(grocery);
+				return next;
+			});
+		}
+	};
+
 	return (
 		<div className="flex flex-col gap-2 rounded-lg border bg-purple-300 p-5">
 			<h2 className="font-bold text-2xl">Weekly List</h2>
@@ -26,13 +46,18 @@ export default function DailyChooser() {
 			<div className="grid h-[80%] grid-cols-2 gap-4 overflow-y-auto">
 				{groceries.map((grocery) => (
 					<Button
-						className="mb-2 flex cursor-pointer items-center justify-between rounded-lg border bg-primary/50 p-3 hover:bg-primary/90"
+						className={cn(
+							"mb-2 flex cursor-pointer items-center justify-between rounded-lg border bg-primary/50 p-3 hover:bg-primary/90",
+							weeklyList.has(grocery) ? "bg-primary text-background" : "",
+						)}
 						key={grocery}
+						onClick={() => updateWeeklyList(grocery)}
 					>
 						{grocery}
 					</Button>
 				))}
 			</div>
+			<p>Selected groceries: {Array.from(weeklyList).join(", ")}</p>
 		</div>
 	);
 }
